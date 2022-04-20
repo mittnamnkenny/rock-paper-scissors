9f1888a675ccf9687289600d99944e6f798c2e05
mittnamnkenny <kenny.appelqvist@gmail.com>
2022-04-20 10:40:41 +0000

    Add event listeners for player icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,3 +6,14 @@ const computerRock = document.getElementById("computer-rock");
 const computerPaper = document.getElementById("computer-paper");
 const computerScissors = document.getElementById("computer-scissors");
 
+rock.addEventListener("click", function() {
+    computerRock.style.visibility = "visible";
+})
+
+paper.addEventListener("click", function() {
+    computerPaper.style.visibility = "visible";
+})
+
+scissors.addEventListener("click", function() {
+    computerScissors.style.visibility = "visible";
+})
