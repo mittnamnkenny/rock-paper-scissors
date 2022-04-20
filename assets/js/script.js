d8693b1c6dbc3f958dfd062a55983e2358cee7cc
mittnamnkenny <kenny.appelqvist@gmail.com>
2022-04-20 10:36:46 +0000

    Declare variables for game area icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-console.log("Hejsan");
+const rock = document.getElementById("player-rock");
+const paper = document.getElementById("player-paper");
+const scissors = document.getElementById("player-scissors");
+
+const computerRock = document.getElementById("computer-rock");
+const computerPaper = document.getElementById("computer-paper");
+const computerScissors = document.getElementById("computer-scissors");
+
