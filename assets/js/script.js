33669161c30f6c486cf0baeb67dcdce62263c933
mittnamnkenny <kenny.appelqvist@gmail.com>
2022-04-20 11:02:07 +0000

    Add game functions for random computer outcome

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,14 +6,52 @@ const computerRock = document.getElementById("computer-rock");
 const computerPaper = document.getElementById("computer-paper");
 const computerScissors = document.getElementById("computer-scissors");
 
+
+
 rock.addEventListener("click", function() {
-    computerRock.style.visibility = "visible";
+    let randomRock = Math.floor(Math.random() * 3) + 1;
+
+    if (randomRock === 1) {
+        computerRock.style.visibility = "visible";
+        console.log("tie");
+    } else if (randomRock === 2) {
+        computerPaper.style.visibility = "visible";
+        console.log("loss");
+    } else {
+        computerScissors.style.visibility = "visible";
+        console.log("win");
+    }
+    
 })
 
 paper.addEventListener("click", function() {
-    computerPaper.style.visibility = "visible";
+    let randomPaper = Math.floor(Math.random() * 3) + 1;
+
+    if (randomPaper === 1) {
+        computerPaper.style.visibility = "visible";
+        console.log("tie");
+    } else if (randomPaper === 2) {
+        computerScissors.style.visibility = "visible";
+        console.log("loss");
+    } else {
+        computerRock.style.visibility = "visible";
+        console.log("win");
+    }
+
 })
 
 scissors.addEventListener("click", function() {
-    computerScissors.style.visibility = "visible";
+    let randomScissors = Math.floor(Math.random() * 3) + 1;
+
+    if (randomScissors === 1) {
+        computerScissors.style.visibility = "visible";
+        console.log("tie");
+    } else if (randomScissors === 2) {
+        computerRock.style.visibility = "visible";
+        console.log("loss");
+    } else {
+        computerPaper.style.visibility = "visible";
+        console.log("win");
+    }
+
 })
