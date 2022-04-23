5bbd5edcc96b75e6f18c7cb6a2f250cbc0def3a8
mittnamnkenny <kenny.appelqvist@gmail.com>
2022-04-23 09:03:46 +0000

    Update after code review

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,101 +26,70 @@ const computerScore = document.getElementById("computer-score");
 let playerCount = 0;
 let computerCount = 0;
 
-rock.addEventListener("click", function() {
-    let randomRock = Math.floor(Math.random() * 3) + 1;
+const characters = [
+    rock,
+    paper,
+    scissors
+];
 
-    if (randomRock === 1) {
-        rock.classList.add("tie");
-        computerRock.classList.add("tie");
+for (let character of characters) {
+    character.addEventListener("click", computeWinner);
+}
+
+function computeWinner(e) {
+    let character = e.target;
+
+    let randomNumber = Math.floor(Math.random() * 3);
+
+    if (randomNumber === 1) {
+        character.classList.add("tie");
+        getComputerOpponent(character, "tie").classList.add("tie");
         tie.classList.add("tie");
-    } else if (randomRock === 2) {
-        rock.classList.add("loss");
-        computerPaper.classList.add("win");
+    } else if (randomNumber === 2) {
+        character.classList.add("loss");
+        getComputerOpponent(character, "win").classList.add("win");
         loss.classList.add("loss");
         computerScore.textContent = ++computerCount;
     } else {
-        rock.classList.add("win");
-        computerScissors.classList.add("loss");
+        character.classList.add("win");
+        getComputerOpponent(character, "loss").classList.add("loss");
         win.classList.add("win");
         playerScore.textContent = ++playerCount;
     }
 
     setTimeout(function() {
-        rock.classList.remove("tie");
-        computerRock.classList.remove("tie");
+        character.classList.remove("tie");
+        getComputerOpponent(character, "tie").classList.remove("tie");
         tie.classList.remove("tie");
-        rock.classList.remove("loss");
-        computerPaper.classList.remove("win");
+        character.classList.remove("loss");
+        getComputerOpponent(character, "win").classList.remove("win");
         loss.classList.remove("loss");
-        rock.classList.remove("win");
-        computerScissors.classList.remove("loss");
+        character.classList.remove("win");
+        getComputerOpponent(character, "loss").classList.remove("loss");
         win.classList.remove("win");
     }, 1200);
-    
-})
+}
 
-paper.addEventListener("click", function() {
-    let randomPaper = Math.floor(Math.random() * 3) + 1;
-
-    if (randomPaper === 1) {
-        paper.classList.add("tie");
-        computerPaper.classList.add("tie");
-        tie.classList.add("tie");
-    } else if (randomPaper === 2) {
-        paper.classList.add("loss");
-        computerScissors.classList.add("win");
-        loss.classList.add("loss");
-        computerScore.textContent = ++computerCount;
+function getComputerOpponent(character, winLoseTie) {
+    if (character.id == "player-rock" && winLoseTie == "tie") {
+        return computerRock;
+    } else if (character.id == "player-rock" && winLoseTie == "win") {
+        return computerPaper;
+    } else if (character.id == "player-rock" && winLoseTie == "loss") {
+        return computerScissors;
+    } else if (character.id == "player-paper" && winLoseTie == "tie") {
+        return computerPaper;
+    } else if (character.id == "player-paper" && winLoseTie == "win") {
+        return computerRock;
+    } else if (character.id == "player-paper" && winLoseTie == "loss") {
+        return computerScissors;
+    } else if (character.id == "player-scissors" && winLoseTie == "tie") {
+        return computerScissors;
+    } else if (character.id == "player-scissors" && winLoseTie == "win") {
+        return computerPaper;
     } else {
-        paper.classList.add("win");
-        computerRock.classList.add("loss");
-        win.classList.add("win");
-        playerScore.textContent = ++playerCount;
+        return computerRock;
     }
+}
 
-    setTimeout(function() {
-        paper.classList.remove("tie");
-        computerPaper.classList.remove("tie");
-        tie.classList.remove("tie");
-        paper.classList.remove("loss");
-        computerScissors.classList.remove("win");
-        loss.classList.remove("loss");
-        paper.classList.remove("win");
-        computerRock.classList.remove("loss");
-        win.classList.remove("win");
-  }, 1200);
 
-})
-
-scissors.addEventListener("click", function() {
-    let randomScissors = Math.floor(Math.random() * 3) + 1;
-
-    if (randomScissors === 1) {
-        scissors.classList.add("tie");
-        computerScissors.classList.add("tie");
-        tie.classList.add("tie");
-    } else if (randomScissors === 2) {
-        scissors.classList.add("loss");
-        computerRock.classList.add("win");
-        loss.classList.add("loss");
-        computerScore.textContent = ++computerCount;
-    } else {
-        scissors.classList.add("win");
-        computerPaper.classList.add("loss");
-        win.classList.add("win");
-        playerScore.textContent = ++playerCount;
-    }
-
-    setTimeout(function() {
-        scissors.classList.remove("tie");
-        computerScissors.classList.remove("tie");
-        tie.classList.remove("tie");
-        scissors.classList.remove("loss");
-        computerRock.classList.remove("win");
-        loss.classList.remove("loss");
-        scissors.classList.remove("win");
-        computerPaper.classList.remove("loss");
-        win.classList.remove("win");
-    }, 1200);
-
-})
