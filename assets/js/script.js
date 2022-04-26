6cb861f7d7715ee9917e25a9a6c9e1466915f59f
mittnamnkenny <kenny.appelqvist@gmail.com>
2022-04-26 08:46:49 +0000

    Add comments to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,13 @@
-// Welcome Page Section
-
+// Welcome Page Variables
 const welcomePage = document.getElementById("welcome-page");
 const playBtn = document.getElementById("play");
 
+// Button to initialise the game
 playBtn.addEventListener("click", function() {
     welcomePage.classList.add("hide");
 })
 
-// Game Page Section
-
+// Game Page Variables
 const rock = document.getElementById("player-rock");
 const paper = document.getElementById("player-paper");
 const scissors = document.getElementById("player-scissors");
@@ -23,22 +22,33 @@ const loss = document.getElementById("loss");
 
 const playerScore = document.getElementById("player-score");
 const computerScore = document.getElementById("computer-score");
+
+// Track player and computer score
 let playerCount = 0;
 let computerCount = 0;
 
+// Array holding player variables
 const characters = [
     rock,
     paper,
     scissors
 ];
 
+// Add event listeners for player characters
 for (let character of characters) {
     character.addEventListener("click", computeWinner);
 }
 
+/**
+ * Function to calculate winner 
+ * based on clicked character above
+ */
 function computeWinner(e) {
+
+    // rock, paper or scissors
     let character = e.target;
 
+    // Random number between 0 and 2
     let randomNumber = Math.floor(Math.random() * 3);
 
     if (randomNumber == 1) {
@@ -49,16 +59,19 @@ function computeWinner(e) {
         character.classList.add("loss");
         getComputerOpponent(character, "win").classList.add("win");
         loss.classList.add("loss");
+        // Increment computer score by 1
         computerScore.textContent = ++computerCount;
         showResult();
     } else {
         character.classList.add("win");
         getComputerOpponent(character, "loss").classList.add("loss");
         win.classList.add("win");
+        // Increment player score by 1
         playerScore.textContent = ++playerCount;
         playerResult.textContent = playerCount;
     }
 
+    // Classlist reset after 1.2 seconds
     setTimeout(function() {
         character.classList.remove("tie");
         getComputerOpponent(character, "tie").classList.remove("tie");
@@ -72,6 +85,10 @@ function computeWinner(e) {
     }, 1200);
 }
 
+/**
+ * Function to return correct computer opponent
+ * based on player outcome above
+ */
 function getComputerOpponent(character, winLoseTie) {
     if (character.id == "player-rock" && winLoseTie == "tie") {
         return computerRock;
@@ -94,13 +111,25 @@ function getComputerOpponent(character, winLoseTie) {
     }
 }
 
-// Result Page Section
-
+// Result Page Variables
 const resultPage = document.getElementById("result-page");
 const playerResult = document.getElementById("player-result");
 const playAgainBtn = document.getElementById("play-again");
 const resultMessage = document.getElementById("result-message");
 
+// Button to start a new game and reset score
+playAgainBtn.addEventListener("click", function() {
+    resultPage.classList.remove("show");
+    playerCount = 0;
+    computerCount = 0;
+    playerScore.innerHTML = "0";
+    computerScore.innerHTML = "0";
+})
+
+/**
+ * Function to show the result page 
+ * with the final results of the game
+ */
 function showResult() {
     if (computerCount == 5) {
         resultPage.classList.add("show");
@@ -116,19 +145,11 @@ function showResult() {
     }
 }
 
-playAgainBtn.addEventListener("click", function() {
-    resultPage.classList.remove("show");
-    playerCount = 0;
-    computerCount = 0;
-    playerScore.innerHTML = "0";
-    computerScore.innerHTML = "0";
-})
-
-// Nav Section
-
+// Nav Variables
 const instructions = document.getElementById("instructions");
 const restart = document.getElementById("restart");
 
+// Button for showing the game instructions whenever the user wants
 instructions.addEventListener("click", function() {
     welcomePage.classList.remove("hide");
     welcomePage.getElementsByTagName("h1")[0].style.display = "none";
@@ -139,6 +160,7 @@ instructions.addEventListener("click", function() {
     }
 })
 
+// Button for restart the game whenever the user wants and reset score and layouts
 restart.addEventListener("click", function() {
     resultPage.classList.remove("show");
     welcomePage.classList.remove("hide");
